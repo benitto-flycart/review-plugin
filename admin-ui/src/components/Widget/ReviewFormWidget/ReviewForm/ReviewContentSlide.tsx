8a1e74dd0b46dfa540af3f5c641fa36ae00a4bba
Nishanth-sebastin <Nishanthsebastinj24cseb@gmail.com>
2024-09-11 11:39:57 +0530

    validation completed for review info page

--- admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewContentSlide.tsx
+++ admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewContentSlide.tsx
@@ -5,14 +5,16 @@ import * as yup from 'yup'
 const ReviewContentSlide = ({handleNextSlide}: any) => {
     const {widget, updateWidgetFields, methods} = useContext<any>(ReviewFormWidgetContext)
     const [reviewText,setReviewText] = useState<string>('')
-    const [errors,setErrors] = useState<any>()
+    const [errors,setErrors] = useState<any>({})
 
     const validationSchema = yup.object().shape({
         review_text: yup.string().required("Please enter a review"),
     })
 
+    console.log(errors)
     const validateReview=()=>{
         validationSchema.validate({review_text:reviewText},{abortEarly:false}).then(()=>{
+            setErrors({})
             handleNextSlide()
         }).catch((validationError)=>{
             const validationErrors = {}
@@ -20,7 +22,6 @@ const ReviewContentSlide = ({handleNextSlide}: any) => {
                 // @ts-ignore
                 validationErrors[e.path] = [e.message]
             });
-            console.log(validationErrors)
             setErrors(validationErrors)
         })
     }
@@ -38,7 +39,7 @@ const ReviewContentSlide = ({handleNextSlide}: any) => {
                 value={reviewText}
                 onChange={(e:any)=>setReviewText(e.target.value)}
                 placeholder={widget.review_content.placeholder}></textarea>
-            {errors ?<span>{errors.review_text[0]}</span> : null}
+            {errors ?<span className={"r_rfw_review_text_error"}>{errors.review_text && errors.review_text[0]}</span> : null}
             <div className={"r_rfw_review_content_continue_btn"}>
                 <button className={"r_rfw_btn"} onClick={validateReview} style={methods.getFooterButtonStyles()}>Continue</button>
             </div>
--- admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewerInfoSlide.tsx
+++ admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewerInfoSlide.tsx
@@ -1,10 +1,46 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {ReviewFormWidgetContext} from "../ReviewFormWidgetContextAPI";
+import {produce} from "immer";
+import * as yup from 'yup'
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 const ReviewerInfoSlide = ({handleNextSlide}: any) => {
 
     const {widget, updateWidgetFields, methods} = useContext<any>(ReviewFormWidgetContext)
+    const [state,setState]=useState({
+        first_name:"",
+        last_name:"",
+        email:""
+    })
+    const [errors,setErrors]=useState<any>({})
 
+    const validationSchema=yup.object().shape({
+        first_name: yup.string().required("Please enter a first name"),
+        last_name: yup.string().required("Please enter a last name"),
+        email: yup.string().email("Enter a valid email address").required("Please enter a email"),
+    })
+
+    const handleUpdateFields=(cb:(draftState:any)=>void)=>{
+        const state=produce((draftState:any)=>{
+            cb(draftState)
+        })
+        setState(state)
+    }
+    console.log(state)
+    const validateReviewInfo=()=>{
+        validationSchema.validate(state,{abortEarly:false}).then(()=>{
+            handleNextSlide()
+            setErrors({})
+        }).catch((validationError)=>{
+            const validationErrors = {}
+            validationError?.inner?.forEach((e: any) => {
+                // @ts-ignore
+                validationErrors[e.path] = [e.message]
+            });
+            setErrors((validationErrors))
+        })
+    }
     return (
         <div className={"r_rfw_reviewer_detail_container"} style={methods.getDialogStyles()}>
                             <span className={"r_rfw_reviewer_detail_title"} style={methods.getReviewerTitleStyles()}>
@@ -13,16 +49,31 @@ const ReviewerInfoSlide = ({handleNextSlide}: any) => {
             <div className={"r_rfw_review_names_info_container"}>
                 <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_first_name"}>
                     <label htmlFor="" style={methods.getReviewerLabelStyles()}>First Name</label>
-                    <input className="r_rfw_input_field" value={""} placeholder={"John Doe"} style={methods.getInputStyles()}/>
+                    <input className="r_rfw_input_field" value={state.first_name} placeholder={"John Doe"} onChange={(e:any)=>{
+                        handleUpdateFields((draftState:any)=>{
+                            draftState.first_name=e.target.value
+                        })
+                    }} style={methods.getInputStyles()}/>
+                    {errors ?<span className={"r_rfw_review_info_error"}>{errors.first_name && errors.first_name[0]}</span> : null}
                 </div>
                 <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_last_name"}>
                     <label htmlFor="" style={methods.getReviewerLabelStyles()}>Last Name</label>
-                    <input className="r_rfw_input_field" value={""} placeholder={"John Doe"} style={methods.getInputStyles()}/>
+                    <input className="r_rfw_input_field" value={state.last_name} onChange={(e:any)=>{
+                        handleUpdateFields((draftState:any)=>{
+                            draftState.last_name=e.target.value
+                        })
+                    }}  placeholder={"John Doe"} style={methods.getInputStyles()}/>
+                    {errors ?<span className={"r_rfw_review_info_error"}>{errors.last_name && errors.last_name[0]}</span> : null}
                 </div>
             </div>
             <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_email"}>
                 <label htmlFor="" style={methods.getReviewerLabelStyles()}>Email</label>
-                <input className="r_rfw_input_field" value={""} placeholder={"Email"} style={methods.getInputStyles()}/>
+                <input className="r_rfw_input_field" value={state.email} onChange={(e:any)=>{
+                    handleUpdateFields((draftState:any)=>{
+                        draftState.email=e.target.value
+                    })
+                }} placeholder={"Email"} style={methods.getInputStyles()}/>
+                {errors ?<span className={"r_rfw_review_info_error"}>{errors.email && errors.email[0]}</span> : null}
             </div>
             <div className={"r_rfw_reviewer_section_note"}>
                 <p style={methods.getReviewerDescriptionStyles()}>By Submitting, I acknowledge the Terms
@@ -30,7 +81,7 @@ const ReviewerInfoSlide = ({handleNextSlide}: any) => {
                     review will be publicly posted and shared online</p>
             </div>
             <div className={"r_rfw_reviewer_continue_btn"}>
-                <button className={"r_rfw_btn"} onClick={handleNextSlide} style={methods.getFooterButtonStyles()}>Continue</button>
+                <button className={"r_rfw_btn"} onClick={validateReviewInfo} style={methods.getFooterButtonStyles()}>Continue</button>
             </div>
         </div>
     )
