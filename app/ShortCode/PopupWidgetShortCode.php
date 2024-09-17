94ef3e8a49f9d931b5c33e470378b1c3f38cf3f9
Nishanth-sebastin <Nishanthsebastinj24cseb@gmail.com>
2024-09-17 17:13:32 +0530

    popup widget design completed

--- app/ShortCode/PopupWidgetShortCode.php
+++ app/ShortCode/PopupWidgetShortCode.php
@@ -7,6 +7,10 @@ use Flycart\Review\App\Helpers\Functions;
 use Flycart\Review\App\Helpers\PluginHelper;
 use Flycart\Review\App\Helpers\WordpressHelper;
 use Flycart\Review\App\Route;
+use Flycart\Review\Core\Controllers\Helpers\Widget\WidgetFactory;
+use Flycart\Review\Core\Models\Widget;
+use Flycart\Review\Package\Request\Request;
+use Flycart\Review\Package\Request\Response;
 
 class PopupWidgetShortCode
 {
@@ -49,5 +53,32 @@ class PopupWidgetShortCode
             '_wp_nonce_key' => 'review_frontend_nonce',
             '_wp_nonce' => WordpressHelper::createNonce('review_frontend_nonce'),
         ];
+    }
+
+    public static function getTemplate(Request $request)
+    {
+        try {
+            $path = F_Review_PLUGIN_PATH . 'resources/templates/popup-widget/';
+
+//            $widgetFactory = new WidgetFactory(Widget::PRODUCT_WIDGET, get_locale(), null);
+//
+//            $widget = $widgetFactory->widget;
+//
+//
+//            $header = $widget->getHeaderLayout(); //$settings['layout']['header_layout'];
+//            $main_content = $widget->getMainContentLayout(); //$settings['layout']['widget_layout'];
+
+            ob_start(); // Start output buffering
+            include $path . 'index.php'; // Include the PHP file
+            $template_content = ob_get_clean();
+
+            Response::success([
+                'template' => $template_content,
+            ]);
+
+        } catch (\Exception|\Error $exception) {
+            PluginHelper::logError('Error Occurred While Processing', [__CLASS__, __FUNCTION__], $exception);
+            return Response::error(Functions::getServerErrorMessage());
+        }
     }
 }
--- resources/templates/popup-widget/popup-widget.php
+++ resources/templates/popup-widget/popup-widget.php
@@ -3,7 +3,9 @@
         position: fixed;
         top: 1rem;
         left: 1rem;
+        z-index: 99999;
     }
+
 </style>
 
 <div id="r_rpw_popup_widget_container_wrapper">
@@ -13,50 +15,6 @@
         <link rel="stylesheet"
               href="<?php echo $popup_widget_font_css ?>">
         <div id="r_puw_container_wrapper">
-            <div class="wd_preview_content review-preview-wrap">
-                <div class="r_puw_container r_puw-sharp"
-                     style="color: rgb(231, 6, 128); background-color: rgb(254, 210, 234); display: block;"><span
-                            class="r_puw_close-icon"
-                            style="color: rgb(231, 6, 128); background-color: rgb(254, 210, 234);"><svg width="15"
-                                                                                                        height="15"
-                                                                                                        viewBox="0 0 15 15"
-                                                                                                        fill="none"
-                                                                                                        xmlns="http://www.w3.org/2000/svg"><path
-                                    d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
-                                    fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg></span>
-                    <div class="r_puw-review_wrapper">
-                        <div class="r_puw-review_image_wrapper"><img src="https://unsplash.it/575/595?t=1"
-                                                                     class="r_puw-review_image"></div>
-                        <div class="r_puw-review-details-wrapper">
-                            <div class="r_puw-review-details-name">Edward</div>
-                            <div class="r_puw-review-details-name"><i
-                                        class="review review-gem review-icon-filled "
-                                        style="font-size: inherit; color: inherit;"></i><i
-                                        class="review review-gem review-icon-filled "
-                                        style="font-size: inherit; color: inherit;"></i><i
-                                        class="review review-gem review-icon-filled "
-                                        style="font-size: inherit; color: inherit;"></i><i
-                                        class="review review-gem review-icon-filled "
-                                        style="font-size: inherit; color: inherit;"></i><i
-                                        class="review review-gem review-icon-filled "
-                                        style="font-size: inherit; color: inherit;"></i></div>
-                            <div title="nice snowboard, awesome" class="r_puw-review-details-content">nice snowboard,
-                                awesome
-                            </div>
-                        </div>
-                        <div data-product-url="" style="color: rgb(109, 3, 61);">
-                            <div class="r_puw-product_details_wrapper">
-                                <div class="r_puw-product_details-img_wrapper"><img
-                                            src="https://cdn.shopify.com/s/files/1/0664/4262/5197/files/snowboard_sky_x50.png?v=1720513923"
-                                            alt="" class="r_puw-product_details-img"></div>
-                                <div title="The Compare at Price Snowboard" class="r_puw-product_details-product_title">
-                                    The Compare at Price S...
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </template>
 </div>
