1836b6b36270544a302b891c7a1146b104073e78
benitto-cartrabbit <benitto@cartrabbit.in>
2024-10-28 06:36:09 +0530

    popup_widget logic splitted

--- scripts/src/popup-widget.ts
+++ scripts/src/popup-widget.ts
@@ -28,31 +28,6 @@ jQuery(document).ready(($) => {
 
     let current_page = 1;
 
-    function flycartReviewToast(title: string, type: string) {
-      let newToast = document.createElement("div");
-      newToast.classList.add("r_f_toast", type);
-
-      let titleElement = document.createElement("div");
-      titleElement.classList.add("r_f_toast_title");
-      titleElement.textContent = title;
-
-      newToast.appendChild(titleElement);
-
-      const notificationContainer = document.querySelector(
-        ".r_f_toast_container",
-      );
-      if (notificationContainer) {
-        notificationContainer.appendChild(newToast);
-      }
-
-      setTimeout(() => {
-        newToast.remove();
-      }, 5000);
-    }
-
-    //@ts-ignore
-    window.flycartReviewToast = flycartReviewToast;
-
     const POPUP_WIDGET: any = {
       init: () => {
         let container: any = shadowRoot.querySelector(
@@ -76,10 +51,7 @@ jQuery(document).ready(($) => {
           })
             .then((response: any) => {
               const response_data = response.data;
-              flycartReviewToast(
-                "popup widget fetched successfully",
-                "success",
-              );
+
               current_page++;
               if (container) {
                 let isHovered = false;
@@ -155,7 +127,6 @@ jQuery(document).ready(($) => {
             .catch((error: any) => {
               console.log(error);
               console.log("error occurred");
-              flycartReviewToast("popup widget error", "success");
             });
         };
 
@@ -165,3 +136,25 @@ jQuery(document).ready(($) => {
     POPUP_WIDGET.init();
   })();
 });
+
+function flycartReviewToast(title: string, type: string) {
+  let newToast = document.createElement("div");
+  newToast.classList.add("r_f_toast", type);
+
+  let titleElement = document.createElement("div");
+  titleElement.classList.add("r_f_toast_title");
+  titleElement.textContent = title;
+
+  newToast.appendChild(titleElement);
+
+  const notificationContainer = document.querySelector(".r_f_toast_container");
+  if (notificationContainer) {
+    notificationContainer.appendChild(newToast);
+  }
+
+  setTimeout(() => {
+    newToast.remove();
+  }, 5000);
+}
+//@ts-ignore
+window.flycartReviewToast = flycartReviewToast;
