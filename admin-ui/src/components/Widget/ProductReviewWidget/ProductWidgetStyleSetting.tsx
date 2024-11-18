c9fff69826f27e179f53b53401056e04021ef0dd
swami1302 <swamii1413@gmail.com>
2024-11-18 16:14:24 +0530

    Fonts collection updated Icons update in Style

--- admin-ui/src/components/Widget/ProductReviewWidget/ProductWidgetStyleSetting.tsx
+++ admin-ui/src/components/Widget/ProductReviewWidget/ProductWidgetStyleSetting.tsx
@@ -214,7 +214,8 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                   </Label>
                 </div>
                 <div>
-                  <svg
+                  <i className="farp farp-sharp frt-text-2xl !frt-font-bold"></i>
+                  {/* <svg
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -229,7 +230,7 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                       d="M24 5H5V24H9V9H24V5Z"
                       fill="currentColor"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
               <div
@@ -255,7 +256,8 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                   </Label>
                 </div>
                 <div>
-                  <svg
+                <i className="farp farp-slightly-rounded frt-text-2xl !frt-font-bold"></i>
+                  {/* <svg
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -270,7 +272,7 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                       d="M24 5H13C8.58173 5 5 8.58172 5 13V24H9V13C9 10.7909 10.7909 9 13 9H24V5Z"
                       fill="currentColor"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
 
@@ -297,7 +299,8 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                   </div>
                 </div>
                 <div>
-                  <svg
+                <i className="farp farp-rounded frt-text-2xl !frt-font-bold"></i>
+                  {/* <svg
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -312,7 +315,7 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                       d="M24 5H17C10.3726 5 5 10.3726 5 17V24H9V17C9 12.5817 12.5817 9 17 9H24V5Z"
                       fill="currentColor"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
 
@@ -339,7 +342,8 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                   </Label>
                 </div>
                 <div>
-                  <svg
+                <i className="farp farp-extra-rounded frt-text-2xl !frt-font-bold"></i>
+                  {/* <svg
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -354,7 +358,7 @@ const ProductWidgetStyleSetting = ({ name }: { name: string }) => {
                       d="M24 5H21C12.1635 5 5 12.1634 5 21V24H9V21C9 14.3726 14.3726 9 21 9H24V5Z"
                       fill="currentColor"
                     ></path>
-                  </svg>
+                  </svg> */}
                 </div>
               </div>
               <div
