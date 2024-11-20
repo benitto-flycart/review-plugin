07ee04bc5bf0f5aeafee46f2a1996728ab114a10
swami1302 <swamii1413@gmail.com>
2024-11-20 22:04:08 +0530

    Info Slide design updated

--- admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewerInfoSlide.tsx
+++ admin-ui/src/components/Widget/ReviewFormWidget/ReviewForm/ReviewerInfoSlide.tsx
@@ -13,7 +13,7 @@ const ReviewerInfoSlide = ({handleNextSlide, errors, reviews, setReviews}: any) 
                             </span>
             <div className={"r_rfw_review_names_info_container"}>
                 <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_first_name"}>
-                    <label htmlFor="">First Name</label>
+                    <label htmlFor="">First Name*</label>
                     <input className="r_rfw_input_field" value={reviews.first_name} placeholder={"John Doe"}
                            onChange={(e: any) => {
                                setReviews({...reviews, first_name: e.target.value})
@@ -21,17 +21,17 @@ const ReviewerInfoSlide = ({handleNextSlide, errors, reviews, setReviews}: any) 
                     {errors ? <span
                         className={"r_rfw_review_info_error"}>{errors.first_name && errors.first_name[0]}</span> : null}
                 </div>
-                <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_last_name"}>
-                    <label htmlFor="">Last Name</label>
-                    <input className="r_rfw_input_field" value={reviews.last_name} onChange={(e: any) => {
-                        setReviews({...reviews, last_name: e.target.value})
-                    }} placeholder={"John Doe"}/>
-                    {errors ? <span
-                        className={"r_rfw_review_info_error"}>{errors.last_name && errors.last_name[0]}</span> : null}
-                </div>
+            </div>
+            <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_last_name"}>
+                <label htmlFor="">Last Name*</label>
+                <input className="r_rfw_input_field" value={reviews.last_name} onChange={(e: any) => {
+                    setReviews({...reviews, last_name: e.target.value})
+                }} placeholder={"John Doe"}/>
+                {errors ? <span
+                    className={"r_rfw_review_info_error"}>{errors.last_name && errors.last_name[0]}</span> : null}
             </div>
             <div className={"r_rfw_input_field_wrapper r_rfw_reviewer_email"}>
-                <label htmlFor="">Email</label>
+                <label htmlFor="">Email*</label>
                 <input className="r_rfw_input_field" value={reviews.email} onChange={(e: any) => {
                     setReviews({...reviews, email: e.target.value})
                 }} placeholder={"Email"}/>
